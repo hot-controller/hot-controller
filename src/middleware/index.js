6d30a3bc62534fc0b9295bea02c2d0a05cc34a30
philipodev <philipw2w@googlemail.com>
2018-03-08 12:25:30 +0100

    send options to callback as 3rd arg

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -8,11 +8,11 @@ const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = function(callback, cwd = process.cwd()) {
   const router = express.Router();
-  getOptions(cwd).then(({ distDir, dir }) => {
+  getOptions(cwd).then(options => {
     router.stack = [];
 
-    const outputDir = path.resolve(cwd, distDir);
-    const controllerDir = path.resolve(cwd, dir);
+    const outputDir = path.resolve(cwd, options.distDir);
+    const controllerDir = path.resolve(cwd, options.dir);
 
     const controllerManager = new ControllerManager(router, { outputDir });
 
@@ -23,12 +23,12 @@ module.exports = function(callback, cwd = process.cwd()) {
       compiler.watch(function(err) {
         if (err === null) {
           controllerManager.reload();
-          callback && callback(router, compiler);
+          callback && callback(router, compiler, options);
         }
       });
     } else {
       controllerManager.load();
-      callback && callback(router, null);
+      callback && callback(router, null, options);
     }
   });
 
