3b60572f285954506640a76efe594ee215c53dd2
philipodev <philipw2w@googlemail.com>
2018-03-08 18:05:45 +0100

    async -> promise in jest env for node 6 support

--- tests/env.js
+++ tests/env.js
@@ -2,23 +2,11 @@ const NodeEnvironment = require('jest-environment-node');
 const express = require('express');
 
 class ControllerEnvironment extends NodeEnvironment {
-  constructor(config) {
-    super(config);
-  }
-
-  async setup() {
-    await super.setup();
-
-    this.global.__APP__ = express();
-    this.global.__COMPILERS__ = [];
-  }
-
-  async teardown() {
-    await super.teardown();
-  }
-
-  runScript(script) {
-    return super.runScript(script);
+  setup() {
+    super.setup().then(() => {
+      this.global.__APP__ = express();
+      this.global.__COMPILERS__ = [];
+    });
   }
 }
 
