4abbce4218d83b3e83d94afad71e862f337fea7c
philipodev <philipodev@gmail.com>
2018-03-12 12:46:45 +0100

    fix failed test

--- tests/build.js
+++ tests/build.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const ControllerCompiler = require('../lib/build');
+const PluginManager = require('../lib/plugin');
 const logger = require('../lib/logger');
 
 const PATHS = ['__tests__/simple', '__tests__/plugins'];
@@ -10,6 +11,7 @@ const PATHS = ['__tests__/simple', '__tests__/plugins'];
   PATHS.forEach(path => {
     let resolvedPath = resolve(__dirname, path);
     new ControllerCompiler({
+      plugins: new PluginManager(),
       controllerDir: resolve(resolvedPath, 'controllers'),
       outputDir: resolve(resolvedPath, 'dist/controllers')
     }).build(() => {
